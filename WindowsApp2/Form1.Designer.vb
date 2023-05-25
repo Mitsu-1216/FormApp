8781d334646097754e29d6c6b1c35edbe28f0b58
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-05-25 21:50:02 +0900

    button

--- WindowsApp2/Form1.Designer.vb
+++ WindowsApp2/Form1.Designer.vb
@@ -24,40 +24,53 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(494, 241)
+        Me.Button1.Font = New System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button1.Location = New System.Drawing.Point(489, 317)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(181, 60)
+        Me.Button1.Size = New System.Drawing.Size(254, 98)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "ボタン"
+        Me.Button1.Text = "Click!"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Label1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(92, 187)
+        Me.Label1.Font = New System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(59, 83)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(50, 18)
+        Me.Label1.Size = New System.Drawing.Size(151, 60)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "ラベル"
+        Me.Label1.Text = "name"
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(59, 206)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(151, 60)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "name"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- WindowsApp2/Form1.vb
+++ WindowsApp2/Form1.vb
@@ -1,4 +1,8 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Hello world"
     End Sub
+
+
 End Class
